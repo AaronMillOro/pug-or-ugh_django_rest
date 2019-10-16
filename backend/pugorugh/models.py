f8c16751c4ec255abcab4657886ce619bc863e10
AaronMan <aaron.ibt@gmail.com>
2019-10-16 02:07:07 +0200

    CODE: Add of models, models registered in admin.py, serializers creation

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -2,3 +2,73 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Dog(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    image_filename = models.FilePathField(path='/images/dogs')
+    breed = models.CharField(max_length=255, blank=True)
+    age = models.IntegerField()
+    gender = models.CharField(
+        max_length = 3,
+        choices = [
+            ('m', 'Male'),
+            ('f', 'Female'),
+            ('u', 'Unknown'),
+        ],
+        blank=True
+    )
+    size = models.CharField(
+        max_length =5,
+        choices = [
+            ('s', 'Small'),
+            ('m', 'Medium'),
+            ('l', 'Large'),
+            ('xl', 'Extra large'),
+            ('u', 'Unknown'),
+        ],
+        blank=True
+    )
+
+
+class UserDog(models.Model):
+    user = models.CharField(max_length=255, unique=True, blank=True)
+    dog = models.ForeignKey(Dog, related_name='user_dog')
+    status =models.CharField(
+        max_length = 2,
+        choices = [
+            ('l', 'Liked'),
+            ('d', 'Disliked'),
+        ],
+        blank=True
+    )
+
+
+class UserPref(models.Model):
+    user = models.ForeignKey(User, related_name='user_pref')
+    age = models.CharField(
+        max_length = 4,
+        choices = [
+            ('b', 'Baby'),
+            ('y', 'Young'),
+            ('a', 'Adult'),
+            ('s', 'Senior'),
+        ],
+        blank=True
+    )
+    gender = models.CharField(
+        max_length = 2,
+        choices = [
+            ('m', 'Male'),
+            ('f', 'Female'),
+        ],
+        blank=True
+    )
+    size = models.CharField(
+        max_length = 4,
+        choices = [
+            ('s', 'Small'),
+            ('m', 'Medium'),
+            ('l', 'Large'),
+            ('xl', 'Extra large'),
+        ],
+        blank=True
+    )
