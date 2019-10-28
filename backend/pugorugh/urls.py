3f03e7683a36bd3749218d7e6b5523917c0965d1
AaronMan <aaron.ibt@gmail.com>
2019-10-28 16:25:47 +0100

    ADD: reset DB after each preferences changes

--- backend/pugorugh/urls.py
+++ backend/pugorugh/urls.py
@@ -8,14 +8,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 from pugorugh.views import UserRegisterView
 from . import views
 
-# API endpoints
+
 urlpatterns = format_suffix_patterns([
     # favicon
     url(r'^favicon\.ico$', RedirectView.as_view(
         url='/static/icons/favicon.ico', permanent=True)),
     # index
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-
     # User endpoints
     url(r'^api/user/login/$', obtain_auth_token,
         name='login-user'),
@@ -23,10 +22,11 @@ urlpatterns = format_suffix_patterns([
         name='register-user'),
     url(r'^api/user/preferences/$', views.UserPrefView.as_view(),
         name='user_prefer'),
-
     # Dogs endpoints
     url(r'^api/dogs/$', views.ListDogsView.as_view(),
         name='list_dogs'),
-    url(r'^api/dog/(?P<pk>\d+)/(?P<status>liked|disliked|undecided)/next/$',
+    url(r'^api/dog/(-?)(?P<pk>\d+)/(?P<status>liked|disliked|undecided)/next/$',
         views.RetrieveChangeStatus.as_view(), name='change_status'),
+    url(r'^api/dogs/undecided/$',
+        views.RetrieveStatus.as_view(), name='retrieve_undecided'),
 ])
--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import (CreateAPIView, ListAPIView,
-                                    RetrieveAPIView, RetrieveUpdateAPIView)
+                                    RetrieveAPIView, RetrieveDestroyAPIView,
+                                    RetrieveUpdateAPIView)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 #from rest_framework.views import APIView
@@ -25,10 +26,12 @@ class UserRegisterView(CreateAPIView):
 class UserPrefView(RetrieveUpdateAPIView):
     """
     User preferences allowing to do GET, POST and PUT
-    Available options are in parenthesis, choose at least one of each category:
+    Choose at least one of each category.
     age : Baby(b), Young(y), Adult(a) and Senior(s)
     gender : Male(m) and Female(f)
     size : Small(s), Medium(m), Large(l) and Extra large(xl)
+    Each time preferences are set the UserDog instances are reseted
+    for the current user
     """
     #permission_classes = (IsAuthenticated,)
     #authentication_classes = (TokenAuthentication,)
@@ -43,6 +46,15 @@ class UserPrefView(RetrieveUpdateAPIView):
             user_pref = models.UserPref.objects.get(user=user.id)
         except models.UserPref.DoesNotExist:
             user_pref = models.UserPref.objects.create(user=user)
+        finally:
+            dogs = models.Dog.objects.filter(
+                gender__in=user_pref.gender.split(','),
+                size__in=user_pref.size.split(','),
+                age__in=convert_dog_age(user_pref.age),
+            )
+            models.UserDog.objects.filter(user=user).delete()
+            for dog in dogs:
+                user_dog = models.UserDog.objects.create(user=user, dog=dog)
         return user_pref
 
 
@@ -62,18 +74,15 @@ class ListDogsView(ListAPIView):
         user = self.request.user
         preferences = models.UserPref.objects.get(user=user.id)
         dogs = models.Dog.objects.filter(
-            gender__in=preferences.gender,
-            size__in=preferences.size,
+            gender__in=preferences.gender.split(','),
+            size__in=preferences.size.split(','),
             age__in=convert_dog_age(preferences.age),
         )
         return dogs
 
 
-#/api/dog/<pk>/<status>/next
+#/api/dog/<pk>/<status>/next/
 class RetrieveChangeStatus(RetrieveUpdateAPIView):
-    """
-    Create or retrieve UserDog instance, default status = undecided
-    """
     #permission_classes = (IsAuthenticated,)
     #authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.AllowAny,)
@@ -82,12 +91,14 @@ class RetrieveChangeStatus(RetrieveUpdateAPIView):
     lookup_field = None
 
     def get_queryset(self):
-        """ Retrieve all dogs based on chosen preferences """
+        """
+        Retrieve dogs based on chosen preferences
+        """
         user = self.request.user
         preferences = models.UserPref.objects.get(user=user.id)
         dogs = models.Dog.objects.filter(
-            gender__in=preferences.gender,
-            size__in=preferences.size,
+            gender__in=preferences.gender.split(','),
+            size__in=preferences.size.split(','),
             age__in=convert_dog_age(preferences.age),
         )
         return dogs
@@ -105,7 +116,7 @@ class RetrieveChangeStatus(RetrieveUpdateAPIView):
         dogs = self.get_queryset()
         dog = get_single_dog(dogs, pk)
 
-        #/api/dog/<pk>/undecided/next
+        #/api/dog/<pk>/undecided/next/
         if status == 'undecided':
             try:
                 user_dog = models.UserDog.objects.get(user=user, dog=dog)
@@ -114,25 +125,26 @@ class RetrieveChangeStatus(RetrieveUpdateAPIView):
             except ObjectDoesNotExist:
                 user_dog = models.UserDog.objects.create(user=user, dog=dog)
 
-        #/api/dog/<pk>/liked/next
+        #/api/dog/<pk>/liked/next/
         elif status == 'liked':
             try:
                 user_dog = models.UserDog.objects.get(user=user, dog=dog)
                 user_dog.status = 'l'
                 user_dog.save()
             except ObjectDoesNotExist:
-                    user_dog = models.UserDog.objects.create(
-                        user=user, dog=dog, status='l')
+                user_dog = models.UserDog.objects.create(
+                    user=user, dog=dog, status='l')
 
-        #/api/dog/<pk>/disliked/next
+        #/api/dog/<pk>/disliked/next/
         elif status == 'disliked':
             try:
                 user_dog = models.UserDog.objects.get(user=user, dog=dog)
                 user_dog.status = 'd'
                 user_dog.save()
             except ObjectDoesNotExist:
-                    user_dog = models.UserDog.objects.create(
-                        user=user, dog=dog, status='d')
+                user_dog = models.UserDog.objects.create(
+                    user=user, dog=dog, status='d')
+
         return dog
 
 
@@ -150,15 +162,69 @@ def convert_dog_age(prefered_age):
     Inspection of data range showed a dogs age between  2 and 96 months
     This function changes the selected preference into numerical ranges
     """
-    prefered_age = prefered_age.split(",")
+    prefered_age = prefered_age.split(',')
     dog_age = []
     for item in prefered_age:
-        if item == "b":
+        if item == 'b':
             dog_age = [x for x in range(1,11)]
-        elif item == "y":
+        elif item == 'y':
             dog_age += [x for x in range(11,30)]
-        elif item == "a":
+        elif item == 'a':
             dog_age += [x for x in range(30,70)]
-        elif item == "s":
+        elif item == 's':
             dog_age += [x for x in range(70,100)]
     return dog_age
+
+
+#/api/dog/<pk>/undecided/
+class RetrieveStatus(ListAPIView):
+    #permission_classes = (IsAuthenticated,)
+    #authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.AllowAny,)
+    queryset = models.Dog.objects.all()
+    serializer_class = serializers.DogSerializer
+    lookup_field = None
+
+    def get_queryset(self):
+        """ Retrieve all dogs based on chosen preferences """
+        user = self.request.user
+        preferences = models.UserPref.objects.get(user=user.id)
+        dogs = models.Dog.objects.filter(
+            gender__in=preferences.gender.split(','),
+            size__in=preferences.size.split(','),
+            age__in=convert_dog_age(preferences.age),
+        )
+        return dogs
+"""
+    def get_object(self):
+
+        Get one dog to display and assign one of the diffent status:
+        undecided, liked or disliked
+        The queryset will be obtained in function of the User preferences
+        No dog can be repeated in the UserDog model
+
+        user = self.request.user
+        pk = self.kwargs.get('pk')
+        status = self.kwargs.get('status')
+        dogs = self.get_queryset()
+        dog = get_single_dog(dogs, pk)
+"""
+
+"""
+    def get_queryset(self):
+        user = self.request.user
+        status = self.kwargs.get('status')
+        if status == 'undecided':
+            user_dog = models.UserDog.objects.filter(user=user, status='u')
+        elif status == 'liked':
+            user_dog = models.UserDog.objects.filter(user=user, status='l')
+        elif status == 'disliked':
+            user_dog = models.UserDog.objects.filter(user=user, status='d')
+        return user_dog
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        dogs = self.get_queryset()
+        dog = get_single_dog(dogs, pk)
+        return dog
+"""
