ba66aa3eb74daa29c49ccf0e9cbe65099fafcdc9
AaronMillOro <aaron.ibt@gmail.com>
2020-03-03 23:44:47 +0100

    :wrench: Add of white noise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
